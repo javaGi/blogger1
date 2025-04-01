38d478237cc0a7afa5350ff8988dce1632768b77
aman <iamjavademo@gmail.com>
2025-04-02 00:43:51 +0530

    Fixed the defect 1234 - submit button was disabled.

---
+++ blogger/src/main/java/com/blog/MainUtil.java
@@ -0,0 +1,19 @@
+package com.blog;
+
+public class MainUtil {
+
+
+
+    public static void main(String[] args) {
+        MainUtil mainUtil = new MainUtil();
+        mainUtil.test();
+
+
+
+    }
+
+    public void test(){
+
+        System.out.println("this is test");
+    }
+}
