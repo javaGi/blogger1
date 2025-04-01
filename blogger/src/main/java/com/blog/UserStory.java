4df914ca1dcb006725279bb27f41fe096a9b3497
aman <iamjavademo@gmail.com>
2025-04-02 01:11:34 +0530

    New User Story is Created.

---
+++ blogger/src/main/java/com/blog/UserStory.java
@@ -0,0 +1,16 @@
+package com.blog;
+
+public class UserStory {
+
+    public  void add(){
+
+        int a =10;
+        int b = 20;
+        System.out.println("Addtion of a and b :" +a+b);
+    }
+
+    public static void main(String[] args) {
+        UserStory u = new UserStory();
+        u.add();
+    }
+}
